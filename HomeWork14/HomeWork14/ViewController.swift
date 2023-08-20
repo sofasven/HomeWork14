24d62590020af1c243d85e501da776141332ea5e
Sofa <sofasven00@gmail.com>
2023-08-20 21:37:26 +0300

    Project created

---
+++ HomeWork14/HomeWork14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork14
+//
+//  Created by Sofa on 20.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
